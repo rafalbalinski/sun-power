00f7c02a20bcbb82ac3f2320ab82b043cabb3189
Rafał Baliński <51541461+rafalbalinski@users.noreply.github.com>
2025-05-31 16:04:12 +0200

    fix: improve navigation
    Co-authored-by: Rafał Baliński <rafal@balinski.dev>

--- src/app/core/navigation/navigation.component.ts
+++ src/app/core/navigation/navigation.component.ts
@@ -1,13 +1,16 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { NavbarComponent } from './ui/navbar/navbar.component';
 import { NavigationStorageService } from '@navigation/data-access/navigation-storage.service';
+import { SidebarComponent } from '@navigation/ui/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-navigation',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, SidebarComponent],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss',
 })
 export class NavigationComponent {
   protected readonly navigationStorage = inject(NavigationStorageService);
+
+  protected readonly isSidebarActive = signal(false);
 }
